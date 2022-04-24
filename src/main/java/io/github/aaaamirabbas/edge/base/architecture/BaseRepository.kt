1560b2d8131d66831d54549628e76181c080bbe2
👾 AMIR ABBAS NAGHDI <naghdi.official@gmail.com>
2022-04-24 14:31:45 +0430

    change baseRepository

--- src/main/java/io/github/aaaamirabbas/edge/base/architecture/BaseRepository.kt
+++ src/main/java/io/github/aaaamirabbas/edge/base/architecture/BaseRepository.kt
@@ -1,8 +1,5 @@
 package io.github.aaaamirabbas.edge.base.architecture
 
-import io.github.aaaamirabbas.edge.domain.model.other.FailureModel
-import io.github.aaaamirabbas.edge.ext.other.IECODE
-import io.github.aaaamirabbas.edge.utils.gson.GsonUtils
 import io.github.aaaamirabbas.edge.utils.operation.OperationResult
 import retrofit2.Response
 
@@ -11,47 +8,6 @@ data class ErrorResponse(
     val message: String?
 )
 
-abstract class BaseRepository(
-    private val gsonUtils: GsonUtils,
-) {
-    suspend fun <T> getResult(call: suspend () -> Response<T>): OperationResult<T> {
-        kotlin.runCatching { call() }.onSuccess { response ->
-            if (response.isSuccessful) {
-                response.body()?.let {
-                    return OperationResult.success(it)
-                }
-            }
-
-            gsonUtils.getSafeObject<ErrorResponse>(
-                response.errorBody()?.string().toString()
-            )
-                .onSuccess {
-                    return OperationResult.failure(
-                        FailureModel(
-                            response.code(),
-                            it.message ?: response.message()
-                        )
-                    )
-                }.onFailure {
-                    return OperationResult.failure(
-                        FailureModel(
-                            response.code(),
-                            response.message()
-                        )
-                    )
-                }
-        }.onFailure { e ->
-            return OperationResult.failure(
-                FailureModel(
-                    IECODE, e.message ?: e.stackTrace.toString()
-                )
-            )
-        }
-
-        return OperationResult.failure(
-            FailureModel(
-                IECODE, "Unknown error"
-            )
-        )
-    }
+abstract class BaseRepository {
+    abstract suspend fun <T> getResult(call: suspend () -> Response<T>): OperationResult<T>
 }
