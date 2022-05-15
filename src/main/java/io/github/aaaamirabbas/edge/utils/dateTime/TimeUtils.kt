d840027bdc52baef22a3be1e386e80b12f7a7aa8
master <naghdi.official@gmail.com>
2022-05-15 21:35:09 +0430

    add timer to TimeUtils

--- src/main/java/io/github/aaaamirabbas/edge/utils/dateTime/TimeUtils.kt
+++ src/main/java/io/github/aaaamirabbas/edge/utils/dateTime/TimeUtils.kt
@@ -1,5 +1,10 @@
 package io.github.aaaamirabbas.edge.utils.dateTime
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+
 object TimeUtils {
 
     fun getCurrentMillis() = System.currentTimeMillis()
@@ -7,4 +12,18 @@ object TimeUtils {
 
     fun getSecond(unixTime: Long) = unixTime / 1000
     fun getUnixTime(second: Long) = second * 1000
+
+    fun getTimerTick(
+            scope: CoroutineScope,
+            periodSecond: Long,
+            totalPeriod: Long,
+            block: suspend (remainingTime: Long) -> Unit
+    ) = scope.launch {
+        var total = totalPeriod
+        while (total >= 0 && isActive) {
+            block(total)
+            delay(periodSecond * 1000)
+            total -= periodSecond
+        }
+    }
 }
