75874bce377c124305a08c46270fdaae170f50d0
master <naghdi.official@gmail.com>
2022-07-20 20:45:37 +0430

    add isPhone and isIR phone method in VerifyUtils

--- src/main/java/io/github/aaaamirabbas/edge/utils/verify/VerifyUtils.kt
+++ src/main/java/io/github/aaaamirabbas/edge/utils/verify/VerifyUtils.kt
@@ -1,10 +1,21 @@
 package io.github.aaaamirabbas.edge.utils.verify
 
 object VerifyUtils {
-    fun isEmailValid(email: String): Boolean {
+    fun isEmail(email: String): Boolean {
         return email.matches(
             "^[A-Za-z0-9+_.-]+@(.+)$".toRegex()
         )
     }
 
+    fun isPhoneNumber(mobileNumber: String): Boolean {
+        return mobileNumber.matches(
+            "^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}\$".toRegex()
+        )
+    }
+
+    fun isIRPhoneNumber(phone: String): Boolean {
+        return isPhoneNumber(phone) && phone.matches(
+            "^(\\+98|0)?9\\d{9}\$".toRegex()
+        )
+    }
 }
